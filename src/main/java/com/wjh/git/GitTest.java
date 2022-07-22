5c46b120bd93f8d04935f747b4c12af5bcf8ea8b
wjh <wjh@gmail.com>
2022-07-22 19:52:27 +0800

    first version

---
+++ src/main/java/com/wjh/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.wjh.git;
+
+/**
+ * @author wjh
+ * @create 2022-07-22 19:49
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
