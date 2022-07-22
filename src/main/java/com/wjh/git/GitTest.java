d911840fe0cdd55573d580afaefc722ca25a5e0f
W <59253544+N00544@users.noreply.github.com>
2022-07-22 20:54:50 +0800

    Update GitTest.java

--- src/main/java/com/wjh/git/GitTest.java
+++ src/main/java/com/wjh/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("afafafas");
         System.out.println("push test");
         System.out.println("push test2");
+        System.out.println("pull test");
     }
 }
